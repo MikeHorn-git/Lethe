12daa569f755b4bdf6ad083fcdb621c65212f96d
MikeHorn-git <MikeHornProton@proton.me>
2025-09-29 11:21:01 +0000

    Add Vagrant

--- devenv.nix
+++ devenv.nix
@@ -2,26 +2,43 @@
 
 {
   # https://devenv.sh/packages/
-  packages = with pkgs; [ git checkmake mdformat nixfmt-classic rubocop ];
+  packages = with pkgs; [
+    git
+    checkmake
+    mdformat
+    nixfmt-classic
+    rubyPackages_3_4.rubocop
+    vagrant
+  ];
 
   # https://devenv.sh/languages/
   languages.c.enable = true;
+  languages.ruby.enable = true;
   languages.shell.enable = true;
 
-  # https://devenv.sh/tasks/
-  tasks = {
-    "lint:run".exec = ''
-      git ls-files --cached --others --exclude-standard 'Makefile' | xargs checkmake Makefile
+  # https://devenv.sh/scripts/
+  scripts = {
+    lint.exec = ''
+      git ls-files --cached --others --exclude-standard '*Makefile' | xargs checkmake Makefile
       git ls-files --cached --others --exclude-standard '*.c' '*.h' | xargs clang-format -i
       mdformat README.md
       nixfmt devenv.nix
       rubocop -A .
       git ls-files --cached --others --exclude-standard '*.sh' | xargs shellcheck
       git ls-files --cached --others --exclude-standard '*.sh' | xargs shfmt -w'';
+    reqs.exec = "vagrant install vagrant-reload";
   };
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    echo "Available commands:"
+    echo " - lint         : Lint the all the repository"
+    echo " - reqs         : Install vagrant plugins"
+  '';
 
   enterTest = ''
     make help
+    vagrant --help
   '';
 
   # https://devenv.sh/git-hooks/
